e51731531c887dd5d431416cd21c20b7b226b9f5
Unknown <panzer1119@gmail.com>
2018-06-26 13:23:48 +0200

    Created stargate_control.lua

---
+++ stargate_control.lua
@@ -0,0 +1,11 @@
+--[[
+
+  Author: Panzer1119
+  
+  Date: Created 26 Jun 2018 - 01:23 PM
+  
+  Original Source: https://github.com/Panzer1119/CCStargate/blob/master/stargate_control.lua
+  
+  Direct Download: https://raw.githubusercontent.com/Panzer1119/CCStargate/master/stargate_control.lua
+
+]]--
