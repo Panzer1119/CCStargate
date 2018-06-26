eaddaa62faa064f7e75ca7833771547c8278d942
Unknown <panzer1119@gmail.com>
2018-06-26 16:06:11 +0200

    Edited files.lon

--- stargate_control.lua
+++ stargate_control.lua
@@ -2,7 +2,7 @@
 
   Author: Panzer1119
   
-  Date: Edited 26 Jun 2018 - 04:02 PM
+  Date: Edited 26 Jun 2018 - 04:05 PM
   
   Original Source: https://github.com/Panzer1119/CCStargate/blob/master/stargate_control.lua
   
