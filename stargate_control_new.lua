8cd76843f02e47602cc7e665a46ce796b4f69844
Paul Hagedorn <panzer1119@gmail.com>
2019-07-08 18:17:57 +0200

    Edited stargate_control_new.lua

--- stargate_control_new.lua
+++ stargate_control_new.lua
@@ -2,7 +2,7 @@
 
   Author: Panzer1119
   
-  Date: Edited 08 Jul 2019 - 05:53 PM
+  Date: Edited 08 Jul 2019 - 06:17 PM
   
   Original Source: https://github.com/Panzer1119/CCStargate/blob/master/stargate_control_new.lua
   
@@ -10,3 +10,24 @@
 
 ]]--
 
+os.loadAPI("lib/utils.lua")
+
+PROGRAM_NAME = "Stargate Control"
+PROGRAM_NAME_SHORT = "SG Control"
+
+sg = peripheral.find("stargate")
+mon = peripheral.find("monitor")
+
+MON_WIDTH = 50
+MON_HEIGHT = 26
+
+width, height = mon.getSize()
+
+if (width ~= MON_WIDTH) then
+	error("Monitor size does not match the requirements! (width is " .. width .. ", but should be " .. MON_WIDTH .. ")")
+end
+if (height ~= MON_HEIGHT) then
+	error("Monitor size does not match the requirements! (height is " .. height .. ", but should be " .. MON_HEIGHT .. ")")
+end
+
+print(textutils.serialise(sg))
