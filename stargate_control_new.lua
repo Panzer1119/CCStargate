c02fb91f2647cf48c2f21579932142d341fadb12
Paul Hagedorn <panzer1119@gmail.com>
2019-07-08 18:44:36 +0200

    Edited stargate_control_new.lua

--- stargate_control_new.lua
+++ stargate_control_new.lua
@@ -2,7 +2,7 @@
 
   Author: Panzer1119
   
-  Date: Edited 08 Jul 2019 - 06:17 PM
+  Date: Edited 08 Jul 2019 - 06:44 PM
   
   Original Source: https://github.com/Panzer1119/CCStargate/blob/master/stargate_control_new.lua
   
@@ -30,4 +30,92 @@ if (height ~= MON_HEIGHT) then
 	error("Monitor size does not match the requirements! (height is " .. height .. ", but should be " .. MON_HEIGHT .. ")")
 end
 
-print(textutils.serialise(sg))
+-- Functions for searching an array for a table BEGIN
+
+function getId(entry)
+	if (entry ~= nil) then
+		return entry.id
+	else
+		return nil
+	end
+end
+
+function getName(entry)
+	if (entry ~= nil) then
+		return entry.name
+	else
+		return nil
+	end
+end
+
+function getAddress(entry)
+	if (entry ~= nil) then
+		return entry.address
+	else
+		return nil
+	end
+end
+
+function getAddressShort(entry)
+	if (entry ~= nil and string.len(entry.address) >= 7) then
+		return string.sub(entry.address, 1, 7)
+	else
+		return nil
+	end
+end
+
+-- Functions for searching an array for a table END
+
+stargates = {}
+history = {}
+settings = {} -- TODO!!!
+
+folder_stargate = "stargate"
+file_stargates = folder_stargate .. "/stargates.lon"
+file_history = folder_stargate .. "/history.lon"
+
+security_allow = "ALLOW"
+security_deny = "DENY"
+security_none = "NONE"
+
+-- ###### LOAD BEGIN
+
+function loadStargates()
+	if (not fs.exists(file_stargates)) then
+		stargates = {}
+		saveStargates()
+	end
+	stargates = utils.readTableFromFile(file_stargates)
+end
+
+function loadHistory()
+	if (not fs.exists(file_history)) then
+		history = {}
+		saveHistory()
+	end
+	history = utils.readTableFromFile(file_history)
+end
+
+function loadAll()
+	loadStargates()
+	loadHistory()
+end
+
+-- ###### LOAD END
+
+-- ###### SAVE BEGIN
+
+function saveStargates()
+	utils.writeTableToFile(file_stargates, stargates)
+end
+
+function saveHistory()
+	utils.writeTableToFile(file_history, history)
+end
+
+function saveAll()
+	saveStargates()
+	saveHistory()
+end
+
+-- ###### SAVE END
