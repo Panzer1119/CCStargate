fd8e798297f904e775ce783ad7460fc3b953fd42
Paul Hagedorn <panzer1119@gmail.com>
2019-07-15 18:44:46 +0200

    Edited stargate_control_new.lua

--- stargate_control_new.lua
+++ stargate_control_new.lua
@@ -2,7 +2,7 @@
 
   Author: Panzer1119
   
-  Date: Edited 13 Jul 2019 - 09:52 PM
+  Date: Edited 15 Jul 2019 - 06:44 PM
   
   Original Source: https://github.com/Panzer1119/CCStargate/blob/master/stargate_control_new.lua
   
